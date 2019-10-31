7a76505bf4aca1c2b94e023188b4a9bbcda86bb9
Tomas Sladecek <sladecekt@gmail.com>
2019-10-31 18:33:24 +0100

    I created a new function that can compute the dph matrices, tail statistic and moments for dph. Also better function for generating sfs

--- R/Segregating_sites.R
+++ R/Segregating_sites.R
@@ -5,6 +5,7 @@
 #' Function to compute reward transformed matrix T* as well as vector of initial probabilities and the defect
 #'
 #' @import partitions
+#' @import dplyr
 #'
 #' @usage rewardtransformparm(n, init_probs, subint_mat)
 #'
@@ -104,54 +105,122 @@ RateMAndStateSpace <- function(n){
 
 #----------------------------------------------------------------------------------------------
 
-#' iton_mats
 #'
-#' Computes T* matrix, initial probabilities and defect for some frequency count (singleton, doubleton, ...)
+#'
+#' Computes P and T* matrix, initial probabilities and defect for some frequency count (singleton, doubleton, ...)
 #'
 #' @usage iton_mats(n)
 #'
 #' for singletons
 #' @usage iton_mats(n, itons = 1, theta = 3)
 
-iton_mats <- function(n, init_probs = c(), itons = 0, theta = 2, rewards = c()){
+discph = function(n, k = 0, pi_vec = NA, itons = 0, theta = 2, moment = F, tail_stat = F){
 
-  ############## Step1: Preparation of Rate matrix (T), initial probabilities (pi) and reward vector ##############
-  matrixes = RateMAndStateSpace(n)
-  # Rate Matrix
-  if(n == 2) {T_table = matrix(matrixes$RateM[1, 1])}
-  else{T_table = matrixes$RateM[1:ncol(matrixes$RateM)-1, 1:ncol(matrixes$RateM)-1]}
+  # All Segregating Sites - This computes the P matrix faster than setting rewards to sum(ri)
+  if(itons == 0){
+    ph = phase_type('T_Total', n = n)
 
-  # Initial Distribution (init_probs)
-  # if nothing is supplied in the function argument, it creates a vector with first entry
-  # being 1 and all the others 0
+    T_table = ph$subint_mat
+    alpha = ph$init_probs
+    defect = 0
 
-  if(length(init_probs) == 0){
-    if(n == 2){init_probs = matrix(c(1))}
-    else{init_probs = c(1, rep(0, nrow(T_table)-1))}
+    P = solve(diag(nrow(T_table)) - 2/theta * T_table)
   }
-  if(length(rewards == 0)){
-  # Specifying if we are considering all segregating sites or something more specific
-    if(itons == 0){ # means all (singletons + doubletons + ...)
-      if(n == 2){reward = matrix(sum(matrixes$StSpM[1,]))}
-      else{reward = apply(matrixes$StSpM[1:nrow(matrixes$StSpM)-1, ], 1, sum)}
-    }
-    else{
-      if(itons < n){
-        if(n == 2){reward = matrix(matrixes$StSpM[1,1])}
-        else{reward = matrixes$StSpM[1:nrow(matrixes$StSpM)-1,][,itons]}
+
+  # Special Case (eg. singletons or tail statistic)
+  else{
+    # There is no reason to proceed if this condition is True
+    if(itons >= n){
+      if(moment){
+        return(list(Moment_1 = 0, Moment_2 = 0, Variance = 0, defect = 0))
       }
       else{return(0)}
     }
+
+    ######## Step1: Preparation of Rate matrix (T), initial Distribution (pi) and reward vector ########
+    matrixes = RateMAndStateSpace(n)
+    # Rate Matrix
+    if(n == 2){
+      T_table = matrix(matrixes$RateM[1, 1])
+    }
+    else{
+      T_table = matrixes$RateM[1:ncol(matrixes$RateM)-1, 1:ncol(matrixes$RateM)-1]
+    }
+
+    # Initial Distribution
+    if(is.na(pi_vec)){
+      if(n == 2){
+        pi_vec = matrix(c(1))
+      }
+      else{
+        pi_vec = c(1, rep(0, nrow(T_table)-1))
+      }
+    }
+
+    ####### REWARDS #######
+    # Tail Statistic
+    if(tail_stat){
+      if(n == (itons + 1)){
+        reward = matrix(matrixes$StSpM[1:(nrow(matrixes$StSpM)-1), ncol(matrixes$StSpM)-1])
+      }
+      else{
+        reward = apply(matrixes$StSpM[1:(nrow(matrixes$StSpM)-1), itons:(ncol(matrixes$StSpM)-1)], 1, sum)
+      }
+    }
+
+    # I-tons
+    else{
+      if(n == 2){
+        reward = matrix(matrixes$StSpM[1,1])
+      }
+      else{
+        reward = matrixes$StSpM[1:nrow(matrixes$StSpM)-1,][,itons]
+      }
+    }
+
+    ######### Step2: Computation of T*, alpha and defect ##########
+    rew_transformed = rewardtransformparm(reward, pi_vec, T_table)
+    alpha = rew_transformed$newinitprob
+    T_star = rew_transformed$newsubintensitymatrix
+    defect = rew_transformed$defect
+
+    ########## Step3: Computation of P and p (transformation to DPH) ##########
+    P = solve(diag(nrow(T_star)) - 2/theta * T_star)
   }
-  ############## Step2: Computation of T*, alpha and defect ##############
-  rew_transformed = rewardtransformparm(reward, init_probs, T_table)
-  alpha = rew_transformed$newinitprob
-  T_star = rew_transformed$newsubintensitymatrix
+  # Calculating The Moments
+  if(moment | tail_stat){
+    m1 = alpha %*% solve(diag(nrow(P)) - P) %*% matrix(1, nrow = nrow(P)) # Corollary 1.2.64
+    m2 = 2 * alpha %*% P %*% (solve(diag(nrow(P)) - P) %*% solve(diag(nrow(P)) - P)) %*% matrix(1, nrow = nrow(P))# Theorem 1.2.69
+    es = m1 - 1 + defect
+    mvar = m2 - es^2 - es
+    return(list(Moment_1 = m1[1], Moment_2 = m2[1], Variance = mvar[1], defect = defect))
+  }
+  # Calculating the Probability P(S = k)
+  else{
+    if(itons == 0){
+      list(P = P, alpha = alpha, defect = defect)
+    }
+    else{
+      list(P = P, T_star = T_star, alpha = alpha, defect = defect)
+    }
+  }
+}
 
-  ############## Step3: Computation of subtransition matrix P ##############
-  P = solve(diag(nrow(T_star)) - 2/theta * T_star)
 
-  list(P = P, T_star = T_star, alpha = alpha, defect = rew_transformed$defect)
+sfs <- function(n_vec, theta = 2){
+  sfs_df <- tibble()
+  # iterate through sample sizes given in n_vec
+  for (i in 1:length(n_vec)){
+
+    # calculate E[ksi_i] and Var[ksi_i] for every i < n
+    for (iton in 1:(n_vec[i]-1)){
+      m = discph(n_vec[i], itons = iton, moment = T, theta = theta)
+      varksi = m$Variance
+      ksi = m$Moment_1 - 1 + m$defect
+      sfs_df <- bind_rows(sfs_df, tibble(n = n_vec[i], itons = iton, E_ksi = ksi, Var_ksi = varksi))
+    }
+  }
+  sfs_df
 }
 
 #----------------------------------------------------------------------------------------------
@@ -172,25 +241,3 @@ dsegsites <- function(x, alpha, P){
   dens_vec
 }
 
-##### Simulated SFS #####
-# in a way wrapper for the above functions
-
-sfs <- function(n, init_probs = c(), theta = 2){
-  sfs_vec <- c()
-
-  if(length(init_probs) == 0){
-    sfs_vec <- theta / 1:n
-  }
-  else{
-    n_max = max(10*theta, n) # this is weird i know. I dont know how else should we control the resolution
-    for (i in 1:(n-1)) {
-      params = iton_mats(n, init_probs, itons = i, theta)
-      sfs_vec = c(sfs_vec, sum(dsegsites(1:n_max, params$alpha, params$P)*(1:n_max)))
-    }
-  }
-  sfs_vec / sum(sfs_vec)
-}
-
-# sfs(15)
-# sfs(15, theta = 5)
-# sfs(10, init_probs = c(0.9, 0.1, rep(0, 39)))
