ab0730361ee3d73c7869f43c4217c21c5a4430f7
rivasiker <ikerrivas96@gmail.com>
2019-11-08 11:20:21 +0100

    I moved the sfs() function to a separate R script, and added the plotting inside of the function. I also changed the vignette accordingly.

--- R/disc_phase_type.R
+++ R/disc_phase_type.R
@@ -194,40 +194,6 @@ summary.disc_phase_type <- function(object, ...) {
 }
 
 
-#' Site Frequency spectrum
-#'
-#' @description Returns a list of expected counts of itons up to n-1. Works with different values of `theta` (population mutation parameter)
-#' as well as with custom vector of initial probabilities.
-#'
-#' @usage sfs(n, theta = 2, init_probs = NULL)
-#'
-#' @param n sample size
-#' @param theta population mutation parameter
-#' @param init_probs vector of initial probabilities. There is no need to supply entire vector if most of its elements are zero. The function computes the rest automatically
-#' @examples sfs(n = 5)
-#' sfs(5, theta = 3)
-#' sfs(10, 2, init_probs = c(0.8, 0.2))
-#'
-#' @export
-sfs <- function(n, theta = 2, init_probs = NULL){
-  E_ksi = rep(NA, n-1)
-  Var_ksi = rep(NA, n-1)
 
-  states = nrow(RateMAndStateSpace(n)$StSpM)-1
-  if(!is.null(init_probs)){
-    if(length(init_probs) != states){
-      message('Zeros added to init_probs vector to make it the right size')
-      init_probs = c(init_probs, rep(0, states - length(init_probs)))
-    }
-  }
-    # calculate E[ksi_i] and Var[ksi_i] for every i < n
-  for (iton in 1:(n-1)){
-    dph = disc_phase_type(n, itons = iton, theta = theta, init_probs = init_probs)
-    E_ksi[iton] = mean(dph) - 1
-    Var_ksi[iton] = var(dph)
-  }
-
-  list(E_ksi = E_ksi, Var_ksi = Var_ksi)
-}
 
 
